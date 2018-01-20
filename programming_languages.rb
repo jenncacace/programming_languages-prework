0d35eb444a2d21ed73d398fe289cca07114b0a5e
Jenn Cacace <jenn.cacace@gmail.com>
2018-01-20 17:51:34 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,11 +1,10 @@
 
-
 def reformat_languages(languages)
   new_hash = {}
   languages.each do |style,language_family|
     language_family.each do |language,type|
-      new_hash[language] = type 
-      if new_hash[language][:style].nil? 
+      if new_hash[language].nil?
+        new_hash[language]= type
         new_hash[language][:style] = [style]
       else 
         new_hash[language][:style].push(style)
